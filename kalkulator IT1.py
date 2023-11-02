0baf6b3bfcc2b8abf3191b33f9119f32a4cea127
Hamid <100763844+hamidettajani@users.noreply.github.com>
2023-11-02 11:03:54 +0100

    upload code to github

---
+++ kalkulator IT1.py
@@ -0,0 +1,29 @@
+print("1.Addisjon (+) :")
+print("2.Subtraksjon (-) :")
+print("3.Multiplikasjon (*) :")
+print("4.Divisjon (/) :")
+    
+def addisjon(x,y):
+    return x + y
+def subtraksjon(x,y):
+    return x - y
+def multiplikasjon(x,y):
+    return x * y
+def divisjon(x,y):
+    return x / y
+
+regnestykke = input("Velg ønsket operatør: ")
+if regnestykke in ('1','2','3','4'):
+    tall1 = int(input("Skriv inn ditt første tall = "))
+    tall2 = int(input("Skriv inn ditt andre tall = "))
+
+    if regnestykke == '1':
+        print(tall1, "+", tall2, "=", addisjon(tall1,tall2))
+    elif regnestykke == '2':
+        print(tall1, "-", tall2, "=", subtraksjon(tall1,tall2))
+    elif regnestykke == '3':
+        print(tall1, "*", tall2, "=", multiplikasjon(tall1,tall2))
+    elif regnestykke == '4':
+        print(tall1, "/", tall2, "=", divisjon(tall1,tall2))
+
+        
