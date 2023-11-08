402f3fe4ad6bdf193efec75807a5508fabc2c6bd
Hamid <100763844+hamidettajani@users.noreply.github.com>
2023-11-08 22:21:58 +0100

    extended versjon

--- kalkulator IT1.py
+++ kalkulator IT1.py
@@ -11,11 +11,14 @@ def multiplikasjon(x,y):
     return x * y
 def divisjon(x,y):
     return x / y
+def addisjon3(x,y,z):
+    return x + y + z
 
 regnestykke = input("Velg ønsket operatør: ")
-if regnestykke in ('1','2','3','4'):
-    tall1 = int(input("Skriv inn ditt første tall = "))
-    tall2 = int(input("Skriv inn ditt andre tall = "))
+if regnestykke in ('1','2','3','4','5'):
+    tall1 = int(input("Skriv inn ditt første tall :"))
+    tall2 = int(input("Skriv inn ditt andre tall :"))
+    tall3 = int(input("Skriv inn ditt tredje tall :"))
 
     if regnestykke == '1':
         print(tall1, "+", tall2, "=", addisjon(tall1,tall2))
@@ -25,5 +28,6 @@ if regnestykke in ('1','2','3','4'):
         print(tall1, "*", tall2, "=", multiplikasjon(tall1,tall2))
     elif regnestykke == '4':
         print(tall1, "/", tall2, "=", divisjon(tall1,tall2))
-
+    elif regnestykke == '5':
+        print(tall1, "+", tall2, "+", tall3, "=", addisjon3(tall1,tall2,tall3))
         
